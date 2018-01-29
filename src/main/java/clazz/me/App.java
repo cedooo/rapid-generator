2694fc90b5db96c693c00d3bfadb0e51efbbe37f
cedo <cedoo@qq.com>
2018-01-29 21:38:54 +0800

    updat to version 3.9.2.20100720

---
+++ src/main/java/clazz/me/App.java
@@ -0,0 +1,13 @@
+package clazz.me;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
